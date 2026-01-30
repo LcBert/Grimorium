a29f9732f3d82dae1806cec1109c72855dc9aa9b
Luca Bertaggia <luca.bertaggia82@gmail.com>
2026-01-30 13:50:53 +0100

    Added altar model

--- common/src/main/java/com/lucab/grimorium/blocks/altar/AltarBlock.java
+++ common/src/main/java/com/lucab/grimorium/blocks/altar/AltarBlock.java
@@ -10,6 +10,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.RenderShape;
@@ -19,13 +20,33 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
 import org.jetbrains.annotations.Nullable;
 
 public class AltarBlock extends BaseEntityBlock {
     public static final MapCodec<AltarBlock> CODEC = simpleCodec(AltarBlock::new);
+    private static VoxelShape shape = Shapes.empty();
 
     public AltarBlock(BlockBehaviour.Properties properties) {
         super(properties);
+        shape = Shapes.join(shape, Shapes.box(0.0625, 0, 0.0625, 0.9375, 0.0625, 0.9375), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.125, 0.0625, 0.125, 0.875, 0.125, 0.875), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.125, 0.1875, 0.8125, 0.1875, 0.8125), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.25, 0.1875, 0.25, 0.75, 0.8125, 0.75), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.0625, 0.9375, 0.0625, 0.9375, 1, 0.125), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.8125, 0.1875, 0.8125, 0.875, 0.8125), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.125, 0.875, 0.125, 0.875, 0.9375, 0.875), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.75, 0.4375, 0.375, 0.875, 0.6875, 0.625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.375, 0.4375, 0.125, 0.625, 0.6875, 0.25), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.125, 0.4375, 0.375, 0.25, 0.6875, 0.625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.375, 0.4375, 0.75, 0.625, 0.6875, 0.875), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.0625, 0.9375, 0.875, 0.9375, 1, 0.9375), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.0625, 0.9375, 0.125, 0.125, 1, 0.875), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.875, 0.9375, 0.125, 0.9375, 1, 0.875), BooleanOp.OR);
     }
 
     @Override
@@ -68,6 +89,11 @@ public class AltarBlock extends BaseEntityBlock {
             }
             super.onRemove(state, level, pos, newState, isMoving);
         }
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return Shapes.or(shape);
     }
 
     @Nullable
